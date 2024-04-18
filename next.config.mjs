5841acfd574e76d13108117eaec77f5e3471f543
alexshenker <alekseyshenker@gmail.com>
2024-04-18 18:05:19 -0400

    updated config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+    },
+    env: {
+        NEXTAUTH_URL:
+            process.env.VERCEL_ENV === "production" &&
+            process.env.VERCEL_URL !== undefined
+                ? "https://homesale.vercel.app"
+                : process.env.VERCEL_URL !== undefined
+                ? `https://${process.env.VERCEL_URL}`
+                : process.env.NEXTAUTH_URL,
 
-export default nextConfig;
+        NEXT_PUBLIC_NEXTAUTH_URL:
+            process.env.VERCEL_ENV === "production" &&
+            process.env.VERCEL_URL !== undefined
+                ? "https://homesale.vercel.app"
+                : process.env.VERCEL_URL !== undefined
+                ? `https://${process.env.VERCEL_URL}`
+                : process.env.NEXT_PUBLIC_NEXTAUTH_URL,
+    },
+};
+
+module.exports = nextConfig;
