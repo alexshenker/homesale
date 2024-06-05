efe09da45c9c9d95c4dd5778b41a19c1280356f4
alexshenker <alekseyshenker@gmail.com>
2024-06-05 11:47:10 -0400

    header style improved

--- src/components/ui/table/cell/Cell.tsx
+++ src/components/ui/table/cell/Cell.tsx
@@ -4,17 +4,17 @@ import { FC, PropsWithChildren } from "react";
 interface Props extends TableCellProps {}
 
 const Cell: FC<PropsWithChildren<Props>> = ({ children, ...props }) => {
-  return (
-    <TableCell
-      {...props}
-      style={{
-        padding: "10px",
-        ...props.style,
-      }}
-    >
-      {children}
-    </TableCell>
-  );
+    return (
+        <TableCell
+            {...props}
+            style={{
+                padding: "10px",
+                ...props.style,
+            }}
+        >
+            {children}
+        </TableCell>
+    );
 };
 
 export default Cell;
