749c124c2150a02799bf49c60ae5a9959f41bedb
alexshenker <alekseyshenker@gmail.com>
2024-06-05 11:45:21 -0400

    table header toggle style improvements

--- src/components/fields/Switch.tsx
+++ src/components/fields/Switch.tsx
@@ -1,16 +1,57 @@
 import { Switch as MUISwitch, Stack, SwitchProps } from "@mui/material";
 import Label from "../ui/label/Label";
+import exhaustiveSwitch from "@/utils/public/exhaustiveSwitch";
 
 export interface Props extends OmitMod<SwitchProps, "checked"> {
     label: React.ReactNode;
     on: boolean;
+    labelPlacement?: "top" | "left" | "right";
 }
 
-const Switch = ({ label, on, ...props }: Props): JSX.Element => {
+const Switch = ({
+    label,
+    on,
+    labelPlacement,
+    ...props
+}: Props): JSX.Element => {
+    const labelPlacementLocal = labelPlacement ?? "right";
+
     return (
-        <Stack direction={"row"} alignItems={"center"}>
-            <MUISwitch checked={on} {...props} />
-            <Label>{label}</Label>
+        <Stack
+            direction={labelPlacementLocal === "top" ? "column" : "row"}
+            alignItems={"center"}
+        >
+            {(() => {
+                switch (labelPlacementLocal) {
+                    case "left": {
+                        return (
+                            <>
+                                <Label>{label}</Label>
+                                <MUISwitch checked={on} {...props} />
+                            </>
+                        );
+                    }
+                    case "right": {
+                        return (
+                            <>
+                                <MUISwitch checked={on} {...props} />
+                                <Label>{label}</Label>
+                            </>
+                        );
+                    }
+                    case "top": {
+                        return (
+                            <>
+                                <Label>{label}</Label>
+                                <MUISwitch checked={on} {...props} />
+                            </>
+                        );
+                    }
+                    default: {
+                        return exhaustiveSwitch(labelPlacementLocal);
+                    }
+                }
+            })()}
         </Stack>
     );
 };
--- src/components/ui/table/ToggleColumns.tsx
+++ src/components/ui/table/ToggleColumns.tsx
@@ -1,6 +1,7 @@
 import { Box } from "@mui/material";
 import Text from "../text/Text";
 import { ColsToShow } from "./Table";
+import Switch from "@/components/fields/Switch";
 
 interface Props<ColName extends string> {
     columns: readonly ColName[];
@@ -12,12 +13,11 @@ const ToggleColumns = <ColName extends string>(
     props: Props<ColName>
 ): JSX.Element => {
     return (
-        <Box padding={2}>
+        <Box>
             {props.columns.map((col) => {
                 const on = props.columnsToShow[col];
                 return (
                     <Box
-                        component="button"
                         key={col}
                         display="flex"
                         gap={1}
@@ -28,12 +28,7 @@ const ToggleColumns = <ColName extends string>(
                             });
                         }}
                     >
-                        <Text>{col}:</Text>
-                        {on ? (
-                            <Text type="success">{"On"}</Text>
-                        ) : (
-                            <Text type="error">{"Off"}</Text>
-                        )}
+                        <Switch on={on} label={col}></Switch>
                     </Box>
                 );
             })}
