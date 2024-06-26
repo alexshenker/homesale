5d92d3b8dc05302ba4e69f4810acddd9464ba5d3
alexshenker <alekseyshenker@gmail.com>
2024-06-26 12:43:46 -0400

    removed inter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import Navbar from "@/components/Navbar";
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import Footer from "@/components/Footer";
 import { Stack } from "@mui/material";
 import company from "@/utils/constants/company";
@@ -11,20 +10,17 @@ import toClassName from "@/utils/public/toClassName";
 import ToastProvider from "@/components/ui/Toast/ToastProvider";
 import SessionProvider from "@/components/providers/SessionProvider";
 
-const inter = Inter({ subsets: ["latin"] });
-
 export const metadata: Metadata = {
     title: {
         default: company,
         template: "%s",
     },
-    description:
-        "Piano accompaniment app designed for stringed instruments such as violin, viola, and cello to enhance practicing and performing",
-    keywords: ["Piano Accompaniment", "Sheet Music"],
+    description: "Connecting home sellers and buyers",
+    keywords: ["home", "rent", "apartment"],
     icons: {
         icon: "/favicon.ico",
     },
-    manifest: `${process.env.NEXT_PUBLIC_NEXTAUTH_URL}/site.webmanifest`,
+    manifest: "/site.webmanifest",
 };
 
 export default async function RootLayout({
@@ -34,7 +30,7 @@ export default async function RootLayout({
 }) {
     return (
         <html lang="en">
-            <body className={toClassName(inter.className, "relative")}>
+            <body className={toClassName("relative")}>
                 <SessionProvider>
                     <TanstackProvider>
                         <ToastProvider>
