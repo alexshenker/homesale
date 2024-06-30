0be03d045a0d0bcdbd91d2911675f00dd598ae35
alexshenker <alekseyshenker@gmail.com>
2024-06-30 15:46:23 -0400

    signed url supports puts

--- src/utils/private/createSignedUrl.ts
+++ src/utils/private/createSignedUrl.ts
@@ -1,18 +1,43 @@
 import "server-only";
-import { GetObjectCommand } from "@aws-sdk/client-s3";
+import { GetObjectCommand, PutObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { s3BucketName } from "./bucketMap";
 import { minutes } from "milliseconds";
 import s3 from "./s3";
 import getWasabiKeyFromUrl from "./getWasabiKeyFromSrc";
+import exhaustiveSwitch from "../public/exhaustiveSwitch";
 
-const createSignedUrl = async (urlToChange: string): Promise<string> => {
+export type SignedUrlMethod = "GET" | "PUT";
+
+const createSignedUrl = async (
+    urlToChange: string,
+    method: SignedUrlMethod,
+    expirationInMinutes: number = 10
+): Promise<string> => {
     const key = getWasabiKeyFromUrl(urlToChange);
 
-    const command = new GetObjectCommand({ Bucket: s3BucketName, Key: key });
+    const command = (() => {
+        switch (method) {
+            case "GET": {
+                return new GetObjectCommand({ Bucket: s3BucketName, Key: key });
+            }
+
+            case "PUT": {
+                return new PutObjectCommand({
+                    Bucket: s3BucketName,
+                    Key: key,
+                    ACL: "public-read",
+                });
+            }
+
+            default: {
+                return exhaustiveSwitch(method);
+            }
+        }
+    })();
 
     const signed = await getSignedUrl(s3, command, {
-        expiresIn: minutes(10),
+        expiresIn: minutes(expirationInMinutes),
     });
 
     return signed;
--- src/utils/private/getWasabiKeyFromSrc.ts
+++ src/utils/private/getWasabiKeyFromSrc.ts
@@ -2,7 +2,7 @@ import "server-only";
 import { s3BucketName } from "./bucketMap";
 
 /**
- * @returns - The Key that can be used to upload files to the location in wasabi that is extracted from the provided src url
+ * @returns - (extracts) The Key that can be used to upload files to the location in wasabi that is extracted from the provided src url
  * @param src - The complete url of a file stored in wasabi
  */
 const getWasabiKeyFromSrc = (src: string) => {
